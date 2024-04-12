606a9171a2d9806610f49670bf97da1e6cf02027
aryansanchiya <asanchiya@gmail.com>
2024-04-12 23:42:44 +0530

    new feature(typesofmangoes)

--- MangoliciousApp/admin.py
+++ MangoliciousApp/admin.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import BuyNow,Details,Cod
+from .models import BuyNow,Details,Cod,RipeMangoes
 
-admin.site.register(BuyNow)
 # admin.site.register(Details)
+admin.site.register(BuyNow)
+admin.site.register(RipeMangoes)
 
 
 
 @admin.register(Details)
 class DetailsAdmin(admin.ModelAdmin):
-    list_display = ('email','name','Mo_No','quant','city')
+    list_display = ('email','name','Mo_No','quant','city','address','typeofmangoes')
     list_filter = ('name','city')
     search_fields = ('name','city')
 
 @admin.register(Cod)
 class CodAdmin(admin.ModelAdmin):
-    list_display = ('email','name','Mo_No','quant','city')
+    list_display = ('email','name','Mo_No','quant','city','address','typeofmangoes')
     list_filter = ('name','city')
     search_fields = ('name','city')
